959edb32a867eeae58825cb50702ec5926095444
IsildurX1 <tanishsharrma22@gmail.com>
2019-04-23 05:00:42 +0530

    Adjusted for new heads

--- A_Closing Merge.py
+++ A_Closing Merge.py
@@ -7,12 +7,13 @@ import os
 
 start_row = 4             # Set first value row (0,1,2....)  4 & 334
 group_size = 5
-last_row = 1473           # Second Latest Day's Available Data's last row 447
+last_row = 447           # Second Latest Day's Available Data's last row 447
 sheet_name = 0            # 0 for Gainers and 1 for Losers
-year = "2018"
+year = "2019"
 path = "A_Experiment_"+year+".xlsx"
 
 data = pd.read_excel(path, sheet_name,None)
+mdict = {'01':'JAN','02':'FEB','03':'MAR','04':'APR','05':'MAY','06':'JUN','07':'JUL','08':'AUG','09':'SEP','10':'OCT','11':'NOV','12':'DEC'}
 
 filex = open("A_Temp.txt","r+")
 for loop in range(start_row,last_row+1,group_size+1):
@@ -20,17 +21,21 @@ for loop in range(start_row,last_row+1,group_size+1):
     y = iter[1]
     date = str(y)[8:10]
     month = str(y)[5:7]
-    file_path = "C_" + date + month + year + ".CSV"
+    file_path = "cm" + date + mdict[month] + year + "bhav.csv"
     df = pd.read_csv(file_path)
-    df.columns = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H']
+    df.columns = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', "M", 'O']
     for iterx in range(5):
         keyx = data.iloc[loop+iterx]
         key = keyx[2]
         if str(key) == "x":
-            y = "0"
+            y = "0\t0\t0\t0"
         else:
-            x = df.loc[df['B'] == str(key)]['C']
-            y = x.iloc[0]
-        filex.write(str(y)+"\n")
+            y = ""
+            x1 = df.loc[df['A'] == str(key)]['C']
+            x2 = df.loc[df['A'] == str(key)]['D']
+            x3 = df.loc[df['A'] == str(key)]['E']
+            x4 = df.loc[df['A'] == str(key)]['F']
+            y = str(x1.iloc[0])+"\t"+str(x2.iloc[0])+"\t"+str(x3.iloc[0])+"\t"+str(x4.iloc[0])
+        filex.write(y+"\n")
     filex.write("\n")
 filex.close()
